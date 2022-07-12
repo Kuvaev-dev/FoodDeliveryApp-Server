c36a64d3dc6f03d19a3ba77a086767a3da53a84b
Kuvaev-dev <likeaboss13371@gmail.com>
2022-07-12 13:28:59 +0300

    Main And Order Detail Activities Logic Added

--- app/src/main/java/kuvaev/mainapp/eatit_server/MainActivity.java
+++ app/src/main/java/kuvaev/mainapp/eatit_server/MainActivity.java
@@ -2,13 +2,31 @@ package kuvaev.mainapp.eatit_server;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSignIn;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        txtSlogan = (TextView)findViewById(R.id.txtslogan);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets() , "fonts/NABILA.TTF");
+        txtSlogan.setTypeface(typeface);
+
+        btnSignIn.setOnClickListener(v -> {
+            Intent signIn = new Intent(MainActivity.this , SignInActivity.class);
+            startActivity(signIn);
+        });
     }
 }
